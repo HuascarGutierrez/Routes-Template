441c61d5cc6cdee481d68fb97980ec5f415b558b
Huascar Gutierrez <huascargutierrezcastro9951591@gmail.com>
2023-11-30 12:02:19 -0400

    links

--- src/About.jsx
+++ src/About.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
-
+import { Link } from 'react-router-dom'
 function About() {
   return (
     <div>
-      <p>About</p>
+      <Link to='/about'>
+      <p>About</p></Link>
     </div>
   )
 }
--- src/Dashboard.jsx
+++ src/Dashboard.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function Dashboard() {
   return (
     <div>
+      <Link to="/dashboard">
       <p>Dashboard</p>
+      </Link>
+
     </div>
   )
 }
